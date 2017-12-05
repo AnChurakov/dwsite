d347258d24784f30dcf77c189fb2c2dc89efcf42
AnChurakov <ownedblackhood@mail.ru>
2017-12-05 19:21:29 +0300

    Добавлен проект

---
+++ дизайнер/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$(function() {
+	$(".owl-carousel").owlCarousel({
+		nav:false,
+		loop:true,
+		responsiveClass:true,
+		autoHeight:true,
+		items:1
+	
+	});
+});
+  
+});
