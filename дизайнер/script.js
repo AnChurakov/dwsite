efad8e347e67e7b020a3edc76f60dde3f7e13b80
Шишкодуй СС <plstsale@gmail.com>
2017-12-05 22:37:32 +0300

    Добавил выдвигающееся меню

--- дизайнер/script.js
+++ дизайнер/script.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
 
-$(function() {
-	$(".owl-carousel").owlCarousel({
-		nav:false,
-		loop:true,
-		responsiveClass:true,
-		autoHeight:true,
-		items:1
-	
+	$(function() {
+		$(".owl-carousel").owlCarousel({
+			nav: false,
+			loop: true,
+			responsiveClass: true,
+			autoHeight: true,
+			items: 1,
+		});
 	});
-});
+	//
+	$("#btnSub").hide();
+	$("#personalInfo").click(function(){
+		if ($("#personalInfo").is(":checked")) {
+			$("#btnSub").show();
+		} else {
+			$("#btnSub").hide();
+		}
+	});
+
+	(function(){
+		$(".burger-btn-src").click(function(){
+			$(".menu").slideToggle("slow");
+		});
+		$(window).resize(function(){
+			if ($(this).width() >= 1024) {
+				$(".menu").show();
+			}
+		});
+	}());
   
 });
