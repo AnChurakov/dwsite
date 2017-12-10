657961f5a7063d7be75086131b0fb7d34ba954f0
AnChurakov <ownedblackhood@mail.ru>
2017-12-10 13:55:27 +0300

    много чего было обновлено

--- дизайнер/script.js
+++ дизайнер/script.js
@@ -18,6 +18,19 @@ $(document).ready(function(){
 			$("#btnSub").hide();
 		}
 	});
+	
+
+	$(".customer").click(function() {
+	
+		$('html, body').animate({ scrollTop: $(".uslugi").offset().top }, 1100)
+	
+	});
+	
+	$(".selects").click(function() {
+	
+		$('html, body').animate({ scrollTop: $(".forms").offset().top }, 1100)
+	
+	});
 
 	(function(){
 		$(".burger-btn-src").click(function(){
